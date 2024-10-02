7b6338ea7b9360516553541291239f931cfa10d5
Tony-StPet <dreamhacker51@icloud.com>
2024-10-02 14:39:00 +0300

    tests add

--- src/test/java/Test2Triangle.java
+++ src/test/java/Test2Triangle.java
@@ -7,11 +7,11 @@ public class Test2Triangle {
 
     @Test
     public void testPerimeter1(){
-        Point a = new Point(0, 2);
-        Point b = new Point(0, 0);
-        Point c = new Point(4, 3);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 3);
+        Point c = new Point(4, 0);
         Triangle abc = new Triangle(a,b,c);
-        Assertions.assertTrue(abc.perimeter()-11.1<0.1);
+        Assertions.assertEquals(12, abc.perimeter());
     }
 
     @Test
@@ -23,5 +23,23 @@ public class Test2Triangle {
         Assertions.assertTrue(abc.perimeter() > 0);
     }
 
+    @Test
+    public void testPerimeter3(){
+        Point a = new Point(0, 0);
+        Point b = new Point(30, 0);
+        Point c = new Point(0, 40);
+        Assertions.assertNotEquals(0, c.distanceTo(a));
+        Assertions.assertNotEquals(0, a.distanceTo(b));
+        Assertions.assertNotEquals(0, b.distanceTo(c));
+    }
+    @Test
+    public void testPerimeter4(){
+        Point a = new Point(0, 2);
+        Point b = new Point(0, 0);
+        Point c = new Point(4, 3);
+        Triangle abc = new Triangle(a,b,c);
+        Triangle t2 = new Triangle(b,c,a);
+        Assertions.assertTrue(abc.perimeter() == t2.perimeter());
+    }
 
 }
